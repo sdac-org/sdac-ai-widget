dad3d75b6ae5c3bb242664fa224ae09a7c8b81a8
Nuno Taborda <nuno@nunotaborda.pt>
2026-02-16 09:58:27 +0000

    Add Docker deployment configuration and update scripts to use cross-env

--- server/index.ts
+++ server/index.ts
@@ -90,14 +90,26 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  const onListen = () => {
+    log(`serving on port ${port}`);
+  };
+
+  if (process.platform === "win32") {
+    httpServer.listen(
+      {
+        port,
+        host: "0.0.0.0",
+      },
+      onListen,
+    );
+  } else {
+    httpServer.listen(
+      {
+        port,
+        host: "0.0.0.0",
+        reusePort: true,
+      },
+      onListen,
+    );
+  }
 })();
